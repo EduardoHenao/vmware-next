09af47733f64fade13ebad83f128ee7e7b6a9d19
Eduardo Henao <ehenao@vmware.com>
2022-01-19 18:54:37 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const { allowedStatusCodes } = require("next/dist/lib/load-custom-routes")
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
